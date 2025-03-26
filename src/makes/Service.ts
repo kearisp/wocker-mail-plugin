711bbd2584683434369d93e07ed4afe8969bf637
Kris Papercut <krispcut@gmail.com>
2025-03-26 20:21:40 +0200

    v1.0.4 (#5)

--- src/makes/Service.ts
+++ src/makes/Service.ts
@@ -1,32 +1,32 @@
-import {Config, ConfigProperties} from "@wocker/core";
-
 import {MAILDEV_TYPE, MAILHOG_TYPE} from "../env";
 
 
 export type ServiceType = typeof MAILDEV_TYPE | typeof MAILHOG_TYPE;
 
-export type ServiceProps = ConfigProperties & {
+export type ServiceProps = {
+    name: string;
     type: ServiceType;
     image?: string;
     imageName?: string;
     imageVersion?: string;
 };
 
-export class Service extends Config<ServiceProps> {
+export class Service {
+    public name: string;
     public type: ServiceType;
     public imageName?: string;
     public imageVersion?: string;
 
     public constructor(props: ServiceProps) {
         const {
+            name,
             type,
             image,
             imageName = image,
             imageVersion
         } = props;
 
-        super(props);
-
+        this.name = name;
         this.type = type;
         this.imageName = imageName;
         this.imageVersion = imageVersion;
@@ -57,5 +57,14 @@ export class Service extends Config<ServiceProps> {
         }
 
         return `${imageName}:${imageVersion}`;
+    }
+
+    public toObject(): ServiceProps {
+        return {
+            name: this.name,
+            type: this.type,
+            imageName: this.imageName,
+            imageVersion: this.imageVersion
+        };
     }
 }
